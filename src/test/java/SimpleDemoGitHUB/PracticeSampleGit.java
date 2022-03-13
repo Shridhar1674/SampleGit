d9ab9dc5f920e2992d146af74010ed003ba9f0f3
acer <acer@LTA1GCO2M8>
2022-03-13 20:17:05 +0530

    Shridhar First Git

---
+++ src/test/java/SimpleDemoGitHUB/PracticeSampleGit.java
@@ -0,0 +1,13 @@
+package SimpleDemoGitHUB;
+
+import org.testng.annotations.Test;
+
+public class PracticeSampleGit 
+{
+	@Test
+	public void practiceSampleGit()
+	{
+		System.out.println("Shridhar First Practice");
+	}
+
+}
